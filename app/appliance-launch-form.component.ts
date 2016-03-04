b61c521cff08ed8720aef3b5e180cefd658b8302
Enis Afgan <afgane@gmail.com>
2016-03-04 08:54:47 -0500

    (WIP) Dynamic load of a component. This is working with only static data and instead it should be able to load from the appliance object at runtime.

--- app/appliance-launch-form.component.ts
+++ app/appliance-launch-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from 'angular2/core';
+import { Component, View, DynamicComponentLoader, ElementRef } from 'angular2/core';
 import {
    FORM_DIRECTIVES,
    FormBuilder,
@@ -6,16 +6,31 @@ import {
    ControlGroup } from 'angular2/common';
 import { Appliance } from './appliance';
 
+
+@Component({
+   selector: 'launch-form',
+   properties: ['greeting'],
+   template: `<i>{{ greeting }}</i>`
+})
+// @View({
+//    template: `
+//     <b>{{ greeting }}</b>
+//   `
+// })
+class SomeComponent {}
+
+
+
 @Component({
    selector: 'appliance-launch-form',
-   template: `
-      <h3>Launch a {{ appliance.name }} appliance</h3>
-      <form [ngFormModel]="launchForm" (submit)="doLaunch($event)">
-         <label for="clusterName">Cluster name</label>:
-         <input ngControl="clusterName" id="clusterName" #name="ngForm">
-         <button type="submit">Launch</button>
-      </form>
-   `,
+   template: `<div #container></div>`,
+   //    <h3>Launch a {{ appliance.name }} appliance</h3>
+   //    <form [ngFormModel]="launchForm" (submit)="doLaunch($event)">
+   //       <label for="clusterName">Cluster name</label>:
+   //       <input ngControl="clusterName" id="clusterName" #name="ngForm">
+   //       <button type="submit">Launch</button>
+   //    </form>
+   // `,
    inputs: ['appliance'],
    directives: [FORM_DIRECTIVES]
 })
@@ -23,16 +38,47 @@ import { Appliance } from './appliance';
 export class ApplianceLaunchFormComponent {
    appliance: Appliance;
    launchForm: any;
+   loader: DynamicComponentLoader;
+   elementRef: ElementRef;
 
-   constructor(fb: FormBuilder) {
+   constructor(loader: DynamicComponentLoader, elementRef: ElementRef,
+               fb: FormBuilder) {
+      this.loader = loader;
+      this.elementRef = elementRef;
+
+      // Some async action (maybe ajax response with html in it)
+      console.log("Appliance: " + this.appliance);
+      setTimeout(() => this.renderTemplate(`
+         <div>
+            <h2>Hello</h2>
+            <launch-form greeting="Oh, hey!"></launch-form>
+         </div>
+      `, [SomeComponent]), 1000);
+
       // This does not work...
       this.launchForm = fb.group({
          'clusterName': ["", Validators.required]
       });
    }
 
+   renderTemplate(template, directives) {
+      this.loader.loadIntoLocation(
+         toComponent(template, directives),
+         this.elementRef,
+         'container'
+      )
+   }
+
    doLaunch(event) {
       console.log(this.launchForm.value);
       event.preventDefault();
    }
+}
+
+function toComponent(template, directives=[]) {
+   @Component({ selector: 'fake-component' })
+   @View({ template, directives })
+   class FakeComponent { }
+
+   return FakeComponent;
 }
