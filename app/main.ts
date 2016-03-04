734053a00449c2c9342e80ffa4cf8b29b248e4a2
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-03-05 01:48:49 +1100

    Reduced number of Rxjs dependencies.

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
-import 'rxjs/Rx';
+import 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 import { bootstrap }    from 'angular2/platform/browser'
 import { AppComponent } from './app.component'
 
