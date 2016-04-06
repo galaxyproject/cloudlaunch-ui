5cb61961ca8ff2775968c22040179adb15772e0e
Enis Afgan <afgane@gmail.com>
2016-04-06 11:45:28 -0400

    WIP CloudMan form definition

--- app/components/plugins/cloudman.component.ts
+++ app/components/plugins/cloudman.component.ts
@@ -7,4 +7,29 @@ import { ConfigPanelComponent } from '../../layouts/config-panel.component';
    inputs: ['application'],
    directives: [ConfigPanelComponent]
 })
-export class CloudManConfigComponent {}
+export class CloudManConfigComponent {
+   cluster: Object = {};
+   clusterTypes: Object[] = [  // First element in the list if the default choice
+      {'value': 'Data', 'title': 'SLURM cluster only'},
+      {'value': 'Galaxy', 'title': 'SLURM cluster with Galaxy'},
+      {'value': 'None', 'title': 'Do not set cluster type now'}]
+   storageType: string = "";
+   showAdvanced: boolean = false;
+
+   onSubmit() {
+      console.log(this.cluster);
+   }
+
+   setStorage(sType) {
+      this.storageType = sType;
+      console.log(this.storageType);
+   }
+
+   toggleAdvanced() {
+      this.showAdvanced = !this.showAdvanced;
+   }
+
+   // launch() {
+   //    console.log("CMConfigComponent launch method");
+   // }
+}
