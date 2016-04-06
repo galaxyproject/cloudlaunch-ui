359e42cb6f3c5d80ccc00a5ff517846118c2c690
Enis Afgan <afgane@gmail.com>
2016-04-06 18:25:45 -0400

    WIP - convert one CloudMan form field to a FormBuilder object; experiment with a config service as a form of storing launch data for the first step of the launch form

--- app/components/plugins/cloudman.component.ts
+++ app/components/plugins/cloudman.component.ts
@@ -1,23 +1,79 @@
 import { Component, View, DynamicComponentLoader, ElementRef } from 'angular2/core';
 import { ConfigPanelComponent } from '../../layouts/config-panel.component';
+import {
+   FORM_DIRECTIVES,
+   FormBuilder,
+   ControlGroup,
+   AbstractControl} from 'angular2/common';
+import { Router, RouteParams } from 'angular2/router';
+// import { CloudManCloud } from './cloudmancloud.component';
+
+// @RouteConfig([
+//    {path: '/cloud', component: CloudManCloud, name: 'CMCloud', useAsDefault: true}
+// ])
+
+class Config {
+   clusterName: string;
+}
+
+export class CloudManConfigService {
+   c: Config;
+
+   storeInfo(cn: string) {
+      let lc = new Config();
+      lc.clusterName = cn;
+      this.c = lc;
+   }
+
+   configInfo() {
+      console.log("CMConfigService");
+      console.log("cn: ", this.c.clusterName);
+      // return this.c;
+   }
+}
 
 @Component({
    selector: 'cloudman-config',
    templateUrl: 'app/components/plugins/cloudman.component.html',
    inputs: ['application'],
-   directives: [ConfigPanelComponent]
+   providers: [CloudManConfigService],
+   directives: [ConfigPanelComponent, FORM_DIRECTIVES]
 })
+
 export class CloudManConfigComponent {
    cluster: Object = {};
    clusterTypes: Object[] = [  // First element in the list if the default choice
       {'value': 'Data', 'title': 'SLURM cluster only'},
       {'value': 'Galaxy', 'title': 'SLURM cluster with Galaxy'},
       {'value': 'None', 'title': 'Do not set cluster type now'}]
-   storageType: string = "";
+   storageType: string = "transient";  // Init default type: transient or volume
    showAdvanced: boolean = false;
 
-   onSubmit() {
-      console.log(this.cluster);
+   clusterForm: ControlGroup;
+   clusterName: AbstractControl;
+   cmConfigService: CloudManConfigService;
+   // clusterConfig: Config;
+   private router: Router;
+   private routeParams: RouteParams;
+   constructor(fb: FormBuilder, configService: CloudManConfigService,
+      cmConfigService: CloudManConfigService, _router: Router,
+      private _routerParams: RouteParams) {
+      this.router = _router;
+      this.routeParams = _routerParams
+      // console.log("routeParams: ", this.routeParams);
+      // this.clusterConfig = cmConfigService.configInfo();
+      this.cmConfigService = cmConfigService;
+      this.clusterForm = fb.group({
+         'cluster_name': ['']
+      });
+
+      this.clusterName = this.clusterForm.controls['cluster_name'];
+   }
+
+   onSubmit(value: string): void {
+      console.log("onSubmit value: ", value);
+      this.cmConfigService.storeInfo(value['cluster_name']);
+      // this.router.navigate(['Launch', {slug: this.routeParams.get('slug')}]);
    }
 
    setStorage(sType) {
