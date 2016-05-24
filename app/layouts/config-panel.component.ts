d73aadfa23605b9e3c2089945091844f441a119b
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-05-24 14:01:25 +0530

    Rearranged UI layout

--- app/layouts/config-panel.component.ts
+++ app/layouts/config-panel.component.ts
@@ -1,8 +1,11 @@
 import { Component } from 'angular2/core';
+import {ViewEncapsulation} from 'angular2/core';
 
 @Component({
   selector: 'config-panel',
   templateUrl: 'app/layouts/config-panel.component.html',
+  styleUrls: ['app/layouts/config-panel.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ConfigPanelComponent {
 }
