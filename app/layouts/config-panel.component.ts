b0ff6868761fe659f6fd050f36284eb5163dd4c1
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-05-24 23:09:42 +0530

    Add back nested component styling

--- app/layouts/config-panel.component.ts
+++ app/layouts/config-panel.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
    selector: 'config-panel',
    templateUrl: 'app/layouts/config-panel.component.html',
+   styleUrls: ['app/layouts/config-panel.component.css'],
+   encapsulation: ViewEncapsulation.None
 })
 export class ConfigPanelComponent {
 }
