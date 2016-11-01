4b6e52196d342f72a62bfb70a39d70630719e055
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-11-01 22:15:34 +0530

    Add appliance detail and cloudlaunch config controls

---
+++ src/app/marketplace/components/appliance-detail-page/appliance-detail-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+// Services
+import { ApplicationService } from '../../../shared/services/application.service';
+
+// Models
+import { Application } from '../../../shared/models/application';
+
+@Component({
+   selector: 'app-appliance-detail-page',
+   templateUrl: './appliance-detail-page.component.html',
+   styleUrls: ['./appliance-detail-page.component.css'],
+   inputs: ['appliance'],
+   host: {'class' : 'ng-animate pageLoadAnimation'}
+})
+export class ApplianceDetailPageComponent implements OnInit {
+   application: Application;
+
+   constructor(
+      private _applicationService: ApplicationService,
+      private _route: ActivatedRoute) {
+   }
+
+   ngOnInit() {
+      this._route.params
+       .map(params => params['slug'])
+       .flatMap(slug => this._applicationService.getApplication(slug))
+       .subscribe(application => this.application = application);
+   }
+}
