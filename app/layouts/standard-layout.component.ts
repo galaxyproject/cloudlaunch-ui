c3b794404cfb701db0a0568fc0675b7eabdd2a7e
Nuwan Goonasekera <nuwan.ag@gmail.com>
2016-11-21 00:22:56 +0530

    Removed unused folder.

--- app/layouts/standard-layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'standard-layout',
-  templateUrl: 'app/layouts/standard-layout.component.html',
-  styleUrls: ['app/layouts/standard-layout.component.css'],
-})
-export class StandardLayoutComponent {
-}
