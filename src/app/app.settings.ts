e82bace51c98b0db65c76f5e153d42ad6e3c20fe
Nuwan Goonasekera <nuwan.ag@gmail.com>
2017-01-21 22:06:03 +0530

    Tweaked API endpoint setting.

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
     public static get CLOUDLAUNCH_SERVER_ROOT(): string { return ''; }
-    public static get CLOUDLAUNCH_API_ENDPOINT(): string { return '/api/v1'; }
+    public static get CLOUDLAUNCH_API_ENDPOINT(): string { return `${AppSettings.CLOUDLAUNCH_SERVER_ROOT}/api/v1`; }
     public static get CLOUDLAUNCH_SUPPORT_LINK(): string { return 'mailto:help@cloudlaunch.org'; }
     public static get CLOUDLAUNCH_GOOGLE_MAPS_KEY(): string { return 'AIzaSyAy6XPTnR8gFEGuzyQNE2Qrz-8b7WcvOcA'; }
 }
