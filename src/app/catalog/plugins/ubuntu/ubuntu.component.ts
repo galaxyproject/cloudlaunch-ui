0e06c091916b0d78edfb543e029999601d68ae16
Marcus Christie <machrist@iu.edu>
2017-08-28 16:30:56 -0400

    Implement `get form` for UbuntuConfigComponent

--- src/app/catalog/plugins/ubuntu/ubuntu.component.ts
+++ src/app/catalog/plugins/ubuntu/ubuntu.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import {
     FormBuilder,
+    FormGroup,
     FormGroupDirective } from '@angular/forms';
 import { BasePluginComponent } from '../base-plugin.component';
 
@@ -11,9 +12,15 @@ import { BasePluginComponent } from '../base-plugin.component';
     inputs: ['cloud', 'initialConfig']
 })
 export class UbuntuConfigComponent extends BasePluginComponent {
+    ubuntuLaunchForm: FormGroup;
 
     constructor(fb: FormBuilder, parentContainer: FormGroupDirective) {
         super(fb, parentContainer);
+        this.ubuntuLaunchForm = fb.group({});
+    }
+
+    get form(): FormGroup {
+        return this.ubuntuLaunchForm;
     }
 
     get configName(): string {
