e8df88314385a0406923ad4e7f721ee4ced8d6d7
Nuwan Goonasekera <nuwan.ag@gmail.com>
2017-12-09 21:14:24 +0530

    Setup travis.yml and fixed e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('cloudlaunch-ui App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toContain('Appliance Catalog');
   });
 });
